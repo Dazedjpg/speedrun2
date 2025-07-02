24ef9a7ec2b39875e420dbe9264a716277316993
Kaeliyaa <mykzrizer@gmail.com>
2025-07-03 00:32:35 +0700

    www

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -7,13 +7,19 @@ class Game extends Model
 {
     protected $table = 'games';
     protected $primaryKey = 'game_id';
+    public $timestamps = false;
+
+    // Tambahkan ini
+    protected $fillable = [
+        'game_title',
+        'cover_image',
+        'description',
+        'created_at', // opsional, jika kamu pakai
+        // tambahkan kolom lain yang ingin kamu izinkan untuk mass assignment
+    ];
 
     public function runs()
     {
         return $this->hasMany(Run::class, 'game_id');
     }
-
-   
-
-    public $timestamps = false; // <-- tambahkan ini
 }
